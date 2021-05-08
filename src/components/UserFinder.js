d4becf203247e923e286ad4ace61c4ff0748fba7
aimon7 <air.ioannis@gmail.com>
2021-05-08 20:29:12 +0300

    Class-based component & context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,14 +2,11 @@ import { Component, Fragment } from 'react';
 
 import classes from './UserFinder.module.css';
 import Users from './Users';
-
-const DUMMY_USERS = [
-    {id: 'u1', name: 'Max'},
-    {id: 'u2', name: 'Manuel'},
-    {id: 'u3', name: 'Julie'},
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
+
     constructor() {
         super();
 
@@ -22,14 +19,14 @@ class UserFinder extends Component {
     componentDidMount() {
         // Send http request...
         this.setState({
-            filteredUsers: DUMMY_USERS
+            filteredUsers: this.context.users
         })
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
-                filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+                filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))
             })
         }
     }
